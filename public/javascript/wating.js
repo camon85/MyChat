ce8c906fa8e1d6d6057ddd12d81e08bb4ac1861e
jooyong <camon85@gmail.com>
2016-03-24 00:36:56 +0900

    닉네임 설정은 내일해야지...

--- public/javascript/wating.js
+++ public/javascript/wating.js
@@ -1,29 +1,31 @@
-var socket = io.connect('http://localhost:3000');
+$(function() {
+    var socket = io.connect('http://localhost:3000');
 
-$('form').submit(function(){
-    var message =  $('#m').val();
+    $('form').submit(function(){
+        var message =  $('#m').val();
 
-    if (message.length == 0) {
+        if (message.length == 0) {
+            $('#m').focus();
+            return false;
+        }
+
+        socket.emit('chat message', { message: message });
+        $('#m').val('');
         $('#m').focus();
         return false;
-    }
+    });
 
-    socket.emit('chat message', { message: message });
-    $('#m').val('');
-    $('#m').focus();
-    return false;
-});
+    socket.on('join', function(data){
+        console.log('join:' + data);
+        $('#messages').append($('<li>').text(data.serverMsg));
+    });
 
-socket.on('join', function(data){
-    console.log('join:' + data);
-    $('#messages').append($('<li>').text(data.serverMsg));
-});
+    socket.on('chat message', function(data){
+        console.log('chat message:' + data);
+        $('#messages').append($('<li>').text(data.nick + ': ' + data.message));
+    });
 
-socket.on('chat message', function(data){
-    console.log('chat message:' + data);
-    $('#messages').append($('<li>').text(data.nick + ': ' + data.message));
-});
-
-socket.on('disconnect', function(data){
-    $('#messages').append($('<li>').text(data.serverMsg));
+    socket.on('disconnect', function(data){
+        $('#messages').append($('<li>').text(data.serverMsg));
+    });
 });
