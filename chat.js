b7a8cfc9f1822d0db9e5559d202f6c899d477857
jooyong <camon85@gmail.com>
2016-03-24 00:15:08 +0900

    - 입장, 퇴장 메시지. - 닉네임 표시

--- chat.js
+++ chat.js
@@ -2,15 +2,20 @@ function listen(server) {
     var io = require('socket.io')(server);
 
     io.on('connection', function (socket) {
-        var roomName = 'room1';
-
+        var roomName = 'room1'; // TODO room 하드코딩 수정
+        socket.nick = socket.id; // TODO : nick 설정
         socket.join(roomName);
 
-        io.to(roomName).emit('join:' + socket.id);
+        io.to(roomName).emit('join', { serverMsg : '>>>' + socket.nick + '<<< 입장'});
 
         socket.on('chat message', function (data) {
+            data.nick = this.id;
             console.log(data);
             io.to(roomName).emit('chat message', data);
+        });
+
+        socket.on('disconnect', function () {
+            io.to(roomName).emit('disconnect', { serverMsg : '---' + socket.nick + '--- 퇴장'});
         });
     });
 }
