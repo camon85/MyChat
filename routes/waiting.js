65ad99ed878ec6971db2705e8675cfa7eefbcb12
camon <camon85@gmail.com>
2016-04-01 00:33:16 +0900

    facebook login 구현

--- routes/waiting.js
+++ routes/waiting.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET waiting listing. */
 router.get('/', function(req, res, next) {
-  res.render('waiting', { title: 'My Chat' });
+  res.render('waiting', { title: 'My Chat', user: req.user });
 });
 
 module.exports = router;
